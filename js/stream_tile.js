011f65bce41252666b3a2bdbde77981cd7e1f873
Gabriel Redig <glredig@gmail.com>
2016-07-16 01:06:16 -0700

    add pagination with page count

--- js/stream_tile.js
+++ js/stream_tile.js
@@ -8,17 +8,23 @@ var StreamTile = (function() {
 
 	StreamTile.prototype.build = function() {
 		this.element = document.createElement('div');
+		this.element.classList.add('stream_tile');
+
+		this.element.style.height = '100px';
 		
 		this.thumb = document.createElement('img');
 		this.thumb.src = this.thumb_url;
+		this.info_container = document.createElement('div');
+
 		this.header = document.createElement('h3');
 		this.header.innerText = this.display_name;
 		this.stream_info = document.createElement('p');
 		this.stream_info.innerText = this.game + ' - ' + this.viewers + ' viewers';
+		this.info_container.appendChild(this.header);
+		this.info_container.appendChild(this.stream_info);
 
 		this.element.appendChild(this.thumb);
-		this.element.appendChild(this.header);
-		this.element.appendChild(this.stream_info);
+		this.element.appendChild(this.info_container);
 	}
 
 	return StreamTile;
--- js/twidget.js
+++ js/twidget.js
@@ -3,8 +3,11 @@ var Twidget = (function() {
 		header,
 		results,
 		btn,
+		field,
 		stream_tiles = [],
-		offset = 0;
+		offset = 0,
+		tag,
+		result_total = 0;
 
 	function init(config) {
 		container = config.element;
@@ -14,37 +17,102 @@ var Twidget = (function() {
 	function buildHTML() {
 		header = document.createElement('div');
 		results = document.createElement('div');
+		field = document.createElement('input');
 		btn = document.createElement('button');
+		pagination = document.createElement('div');
+		result_count = document.createElement('div');
+		page_nav = document.createElement('div');
+		left_arr = document.createElement('div');
+		left_arr.innerText = '<';
+		page_count = document.createElement('div');
+		right_arr = document.createElement('div');
+		right_arr.innerText = '>';
 
 		container.classList.add('twidget_container');
 		header.classList.add('twidget_header');
 		results.classList.add('twidget_results');
+		pagination.classList.add('twidget_pagination');
+		pagination.style.visibility = 'hidden';
+
+		field.type = 'text';
 
 		btn.classList.add('twidget_button');
 		btn.innerText = 'Search';
 
-		btn.addEventListener('click', function() {
-			search('starcraft');
-		});
-
+		header.appendChild(field);
 		header.appendChild(btn);
+
+		page_nav.appendChild(right_arr);
+		page_nav.appendChild(page_count);
+		page_nav.appendChild(left_arr);
+
+		pagination.appendChild(result_count);
+		pagination.appendChild(page_nav);
+
+		results.appendChild(pagination);
 
 		container.appendChild(header);
 		container.appendChild(results);
 
+		setupListeners();
+
 	}
 
-	function search(keyword) {
-		var url = 'https://api.twitch.tv/kraken/search/streams?limit=5&offset=' + offset + '&q=' + keyword + '&callback=Twidget.display';
+	function setupListeners() {
+		btn.addEventListener('click', function() {
+			search(encodeURI(field.value));
+		});
 
-		var tag = document.createElement('script');
+		field.addEventListener('keyup', function(e) {
+			if (e.keyCode === 13) {
+				search(encodeURI(field.value));
+			}
+		});
+
+		left_arr.addEventListener('click', function() {
+			if (offset > 0) {
+				pageChange(-1);
+			}
+		});
+
+		right_arr.addEventListener('click', function() {
+			if ((offset / 5 + 1) < Math.ceil(result_total / 5)) {
+				pageChange(1);
+			}
+		});
+
+	}
+
+	function pageChange(direction) {
+		if (direction > 0) {
+			offset += 5;
+			search(field.value);
+		}
+		else {
+			offset -= 5;
+			search(field.value);
+		}
+	}
+
+	function search(keyword) {		
+		var url = 'https://api.twitch.tv/kraken/search/streams?limit=5&offset=' + offset + '&q=' + keyword + '&callback=Twidget.display';
+		
+		if (tag !== undefined) {
+			document.body.removeChild(tag);
+		}	
+
+		tag = document.createElement('script');
 		tag.src = url;
 		tag.type = 'text/javascript';
 		document.body.appendChild(tag);
 	}
 
 	function display(data) {
+		result_total = data._total;
 		clearResults();
+		result_count.innerText = 'Total results: ' + result_total;
+		page_count.innerText = (offset / 5 + 1) + '/' + Math.ceil(result_total / 5);
+		pagination.style.visibility = 'visible';
 
 		for (var i = 0; i < data.streams.length; i++) {
 			var stream_tile = new StreamTile({
